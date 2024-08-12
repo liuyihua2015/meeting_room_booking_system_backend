0e582527b336e1e66fbe041714b2fe499bc63996
liuyihua <liuyihua@shinsson.com>
2024-08-12 15:39:30 +0800

    feat: 使用 LoginGuard、PermissionGuard 来做登录和权限的鉴权

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { RequireLogin, RequirePermission, UserInfo } from './custom.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get('aaa')
+  // @SetMetadata('require-login', true)
+  // @SetMetadata('require-permission', ['ddd'])
+  @RequireLogin()
+  @RequirePermission('ddd')
+  aaa(@UserInfo('username') username: string, @UserInfo() userInfo) {
+    console.log(username);
+    console.log(userInfo);
+    return 'aaa';
+  }
+  @Get('bbb')
+  bbb(): string {
+    return 'bbb';
+  }
+  @Get('ccc')
+  // @SetMetadata('require-login', true)
+  // @SetMetadata('require-permission', ['ddd'])
+  @RequireLogin()
+  @RequirePermission('ddd')
+  ccc(): string {
+    return 'ccc';
+  }
 }
