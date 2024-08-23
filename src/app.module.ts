4899e71578f46f4f77620818c31bd875d1fcd1ad
liuyihua <liuyihua@shinsson.com>
2024-08-23 10:34:36 +0800

    feat: 集成了日志框架 winston

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,13 +19,19 @@ import { BookingModule } from './booking/booking.module';
 import { Booking } from './booking/entities/booking.entity';
 import { MinioModule } from './minio/minio.module';
 import * as path from 'path';
-``;
+import 'winston-daily-rotate-file';
+
+import {
+  utilities,
+  WINSTON_MODULE_NEST_PROVIDER,
+  WinstonLogger,
+  WinstonModule,
+} from 'nest-winston';
+import * as winston from 'winston';
+import { CustomTypeOrmLogger } from './CustomTypeOrmLogger';
+
 @Module({
   imports: [
-    // ConfigModule.forRoot({
-    //   isGlobal: true,
-    //   envFilePath: 'src/.env',
-    // }),
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: [
@@ -35,7 +41,7 @@ import * as path from 'path';
     }),
 
     TypeOrmModule.forRootAsync({
-      useFactory(configService: ConfigService) {
+      useFactory(configService: ConfigService, logger: WinstonLogger) {
         return {
           type: 'mysql',
           host: configService.get('mysql_server_host'),
@@ -45,6 +51,7 @@ import * as path from 'path';
           database: configService.get('mysql_server_database'),
           synchronize: false,
           logging: true,
+          logger: new CustomTypeOrmLogger(logger),
           entities: [User, Role, Permission, MeetingRoom, Booking],
           poolSize: 10,
           connectorPackage: 'mysql2',
@@ -54,7 +61,7 @@ import * as path from 'path';
           },
         };
       },
-      inject: [ConfigService],
+      inject: [ConfigService, WINSTON_MODULE_NEST_PROVIDER],
     }),
     JwtModule.registerAsync({
       global: true,
@@ -68,6 +75,34 @@ import * as path from 'path';
       },
       inject: [ConfigService],
     }),
+    WinstonModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        level: 'debug',
+        transports: [
+          new winston.transports.DailyRotateFile({
+            level: configService.get('winston_log_level'),
+            dirname: configService.get('winston_log_dirname'),
+            filename: configService.get('winston_log_filename'),
+            datePattern: configService.get('winston_log_date_pattern'),
+            maxSize: configService.get('winston_log_max_size'),
+          }),
+          new winston.transports.Console({
+            format: winston.format.combine(
+              winston.format.timestamp(),
+              utilities.format.nestLike(),
+            ),
+          }),
+          // 给日志服务器发送日志
+          // new winston.transports.Http({
+          //   host: 'localhost',
+          //   port: 3002,
+          //   path: '/log',
+          // }),
+        ],
+      }),
+      inject: [ConfigService],
+    }),
+
     UserModule,
     RedisModule,
     EmailModule,
--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import { CustomExceptionFilter } from './custom-exception.filter';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import * as cookieParser from 'cookie-parser';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -34,6 +35,8 @@ async function bootstrap() {
   app.enableCors();
 
   app.use(cookieParser());
+
+  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
 
   const configService = app.get(ConfigService);
   await app.listen(configService.get('nest_server_port'));
